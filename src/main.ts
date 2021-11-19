90ad6c35295fbef5cae9a24dc67eb7b25e61ee74
sakho13 <wecl.khotaro@gmail.com>
2021-11-20 02:06:03 +0900

    Update: firebaseインスト

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from './firebaseConfig';
 
 Vue.config.productionTip = false
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 new Vue({
   router,
